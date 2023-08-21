d5d26c9f3928cc592d1b81bbd00fcddb38d858ee
Saleh Shahverdiyev <salehsahverdiyev201@gmail.com>
2023-08-21 16:17:55 +0400

    import css file for styling

--- src/components/layout/auth/index.tsx
+++ src/components/layout/auth/index.tsx
@@ -1,15 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import "./auth.css"
 
 export const AuthLayout = () => {
 	return (
 		<>
 			<header>
-				<h1>this is auth layout</h1>
+				<h1>This is auth layout.</h1>
 			</header>
 			<main>
 				<Outlet />
 			</main>
-			<footer>&copy; {new Date().getFullYear()}</footer>
+			
 		</>
 	);
 };
